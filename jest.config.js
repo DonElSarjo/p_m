2e4d31164d5c119d1203c66864f42aac5df93e90
hermann.denk <hr.denk@gmail.com>
2025-02-18 11:47:38 +0100

    first test done

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],  // Only look for tests in /test directory
+  roots: ['<rootDir>/test'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  setupFilesAfterEnv: ['<rootDir>/test/setup/setupTests.ts'], // Run setup before tests
+  setupFilesAfterEnv: ['<rootDir>/test/setup/setupTests.ts'],
   clearMocks: true,
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',  // ✅ Allows alias imports like @/controllers/users.controller
+    '^@/(.*)$': '<rootDir>/src/$1', 
   }
 };
